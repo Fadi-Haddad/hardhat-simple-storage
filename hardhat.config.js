24ccbcda9370ff9373a8e5965929167b89c4027d
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2024-05-15 23:04:15 +0300

    Change default hardhat settings to sepolia on Alchemy Naas

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,19 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+const Sepolia_RPC_URL = process.env.RPC_URL
+
+const Sepolia_PRIVATE_KEY=process.env.PRIVATE_KEY
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.24",
+  defaultNetwork: "hardhat",
+  networks:{
+    sepolia:{
+      url: Sepolia_RPC_URL,
+      accounts:[Sepolia_PRIVATE_KEY],
+      chainId:11155111,
+    },
+  },
+  solidity: "0.8.7",
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,10 +1,12 @@
 const { ethers } = require("hardhat");
+require("dotenv").config();
 
 async function main() {
     const simpleStoragefactory = await ethers.getContractFactory("SimpleStorage");
-    const simpleStorage = await simpleStoragefactory.deploy();
-    await simpleStorage.deployed();
-}
+    const simpleStorage = await simpleStoragefactory.deploy(); // this code deploys contracts to Hardhat's default network and settings
+    // await simpleStorage.deployed();     //deprecated in hardhat v6
+    console.log(simpleStorage.target);
+  }
 main()
   .then(() => process.exit(0))
   .catch((error) => {
