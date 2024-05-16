da32d8414ec17bc76ace99edfd59bee8d9917b04
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2024-05-16 15:28:04 +0300

    Add verify function to verify contract on etherscan.io

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,4 +1,4 @@
-const { ethers } = require("hardhat");
+const { ethers, run } = require("hardhat");
 require("dotenv").config();
 
 async function main() {
@@ -6,6 +6,14 @@ async function main() {
     const simpleStorage = await simpleStoragefactory.deploy(); // this code deploys contracts to Hardhat's default network and settings
     // await simpleStorage.deployed();     //deprecated in hardhat v6
     console.log(simpleStorage.target);
+  }
+  async function verify(contractAddress, args){ // verify the contract on etherscan.io using their API service, 
+    console.log("verifying contract on etherscan...");
+    await run("verify:verify",{
+      address: contractAddress,
+      arguments: args,
+    });
+
   }
 main()
   .then(() => process.exit(0))
