181da592e167eb3dfa95d3f40bb26ee1ef9af943
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2024-05-16 15:14:48 +0300

    Add etherscan API key to enable contract programatic verification

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,12 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
+require("@nomiclabs/hardhat-etherscan");
 
 const Sepolia_RPC_URL = process.env.RPC_URL
 
 const Sepolia_PRIVATE_KEY=process.env.PRIVATE_KEY
+
+const ETHRSCAN_API_KEY = process.env.ETHRSCAN_API_KEY
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -16,4 +19,8 @@ module.exports = {
     },
   },
   solidity: "0.8.7",
-};
+  etherscan:{
+    apikey :ETHRSCAN_API_KEY, // obtained from etherscan.io to enable programmatic contract verification
+  }
+}
+
